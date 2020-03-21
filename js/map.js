bee43d9b8cd9389c1c4bf2da3b49509bfb8bf892
LAY Frederic <frederic.lay@etu.parisdescartes.fr>
2020-03-21 01:59:12 +0100

    Tableau recap + données parse
    Lancer avec la commande npx http-server dans le cmd
    (localhost:8080)

--- js/map.js
+++ js/map.js
@@ -1,10 +1,11 @@
 import "https://api.mapbox.com/mapbox-gl-js/v1.8.0/mapbox-gl.js";
 
+//API KEY
 const mapbox_token =
 	"pk.eyJ1Ijoic25vd2RlciIsImEiOiJjazgwNnlpZGEwMmk4M2RvZGZtMGo1N2drIn0.SVInkbG2WYBRLNp1Zt5frQ";
 
 mapboxgl.accessToken = mapbox_token;
-
+// création de la map
 var map = new mapboxgl.Map({
 	container: "map",
 	style: "mapbox://styles/mapbox/dark-v10",
@@ -12,6 +13,8 @@ var map = new mapboxgl.Map({
 	center: [0, 20]
 });
 
+// Couleur du marker selon le nb d'infectés
+//(obsolete comme il y'en a trop mtn)
 const getColorFromCount = count => {
 	if (count >= 100) {
 		return "red";
@@ -22,6 +25,8 @@ const getColorFromCount = count => {
 	return "gray";
 };
 
+// requête vers le fichier json qu'on a recuperer du site coronaAPP car API Payant
+// données pas à jour
 fetch("/get-latest.json")
 	.then(response => response.json())
 	.then(data => {
@@ -44,6 +49,40 @@ fetch("/get-latest.json")
 	});
 
 // Avoir les données d'infectés en direct
+//Decalring the Different Variable and Objects
+let new_cases = document.getElementById("new_case");
+let new_death = document.getElementById("new_death");
+let total_death = document.getElementById("total_death");
+let total_recovered = document.getElementById("total_recovered");
+let total_cases = document.getElementById("total_cases");
+let table = document.getElementById("countries_stat");
+// Fetching the Data from the server
+
+//Fetching the World Data
+
+var settings = {
+	async: true,
+	crossDomain: true,
+	url: "https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php",
+	method: "GET",
+	headers: {
+		"x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+		"x-rapidapi-key": "6bc7479adfmshbb660935776cbdbp1d7cd9jsn4d267cfbdc8b"
+	}
+};
+
+$.ajax(settings).done(function(response) {
+	console.log(response);
+	var data = JSON.parse(response);
+	total_cases.innerHTML = data.total_cases;
+	new_cases.innerHTML = data.new_cases;
+	new_death.innerHTML = data.new_deaths;
+	total_death.innerHTML = data.total_deaths;
+	total_recovered.innerHTML = data.total_recovered;
+});
+
+//Fetching The Case by Country Data
+
 var settings = {
 	async: true,
 	crossDomain: true,
@@ -58,4 +97,22 @@ var settings = {
 
 $.ajax(settings).done(function(response) {
 	console.log(response);
+	var data = JSON.parse(response);
+	let countries_stat = data.countries_stat;
+	//Getting all the country statistic using a loop
+	for (let i = 0; i < countries_stat.length; i++) {
+		console.log(countries_stat[i]);
+		//we will start by inserting the new rows inside our table
+		let row = table.insertRow(i + 1);
+		let country_name = row.insertCell(0);
+		let cases = row.insertCell(1);
+		let deaths = row.insertCell(2);
+		let serious_critical = row.insertCell(3);
+		let recovered_per_country = row.insertCell(4);
+		country_name.innerHTML = countries_stat[i].country_name;
+		cases.innerHTML = countries_stat[i].cases;
+		deaths.innerHTML = countries_stat[i].deaths;
+		serious_critical.innerHTML = countries_stat[i].serious_critical;
+		recovered_per_country.innerHTML = countries_stat[i].total_recovered;
+	}
 });
