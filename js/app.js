667cd579b42efcf41586047406c3e5a040e031df
LAY Frederic <frederic.lay@etu.parisdescartes.fr>
2020-03-23 13:47:27 +0100

    login firebase (à refaire)

--- js/app.js
+++ js/app.js
@@ -1,33 +1,56 @@
-var paris = [48.8534, 2.3488];
-//création de la map
-var map = L.map("map").setView([48.8534, 2.3488], 6);
-//création du calque images
-L.tileLayer(
-	"https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
-	{
-		maxZoom: 20
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyDb-Vj_8Ej21Eul6p878XHWmpBe5XNBnAc",
+	authDomain: "coronatracker-67aae.firebaseapp.com",
+	databaseURL: "https://coronatracker-67aae.firebaseio.com",
+	projectId: "coronatracker-67aae",
+	storageBucket: "coronatracker-67aae.appspot.com",
+	messagingSenderId: "804146612879",
+	appId: "1:804146612879:web:80a8d5440768df9a7e3efa",
+	measurementId: "G-RN7ZVMWYCG"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+function signUp() {
+	var email = document.getElementById("email");
+	var password = document.getElementById("password");
+
+	const promise = auth.createUserWithEmailAndPassword(
+		email.value,
+		password.value
+	);
+	promise.catch(e => alert(e.message));
+
+	alert("Signed Up");
+}
+
+function signIn() {
+	var email = document.getElementById("email");
+	var password = document.getElementById("password");
+
+	const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+	promise.catch(e => alert(e.message));
+	alert("Signed In");
+	window.location.href = "map.html";
+}
+
+function signOut() {
+	auth.signOut();
+	alert("Signed Out");
+}
+
+auth.onAuthStateChanged(function(user) {
+	if (user) {
+		var email = user.email;
+		confirm("Active User " + email);
+
+		//Take user to a different or home page
+		window.location.href = "map.html";
+		//is signed in
+	} else {
+		alert("No Active User");
+		//no user is signed in
 	}
-).addTo(map);
-
-// ajout d'un marker
-
-var marker = L.marker(paris).addTo(map);
-//ajout d'un popup
-marker.bindPopup("<h3> Paris,France.</h3>");
-
-fetch("/get-latest.json")
-	.then(response => response.json())
-	.then(data => {
-		// const places = data.places;
-		// const reports = data.reports;
-		const { places, reports } = data;
-		console.log(places, reports);
-		reports
-			.filter(report => !report.hide)
-			.forEach(report => {
-				const { infected, placeId } = report;
-				const currentPlace = places.find(place => place.id === placeId);
-				console.log(infected, currentPlace);
-				L.marker([currentPlace.longitude, currentPlace.latitude]).addTo(map);
-			});
-	});
+});
